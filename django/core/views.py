aa00c93f8edbefda6a869c60ea774657d271eba9
Muzzammil Hussain <muzzammilz@gmail.com>
2019-04-22 17:48:16 +0500

    Template page for movie detail and bootstrap based base template.

--- django/core/views.py
+++ django/core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from core.models import Movie
 
 
 class MovieList(ListView):
+    model = Movie
+
+
+class MovieDetail(DetailView):
     model = Movie
